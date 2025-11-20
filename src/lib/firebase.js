1f57772101a95ea36d5d0cd0ea18857a63a786ef
Timur <akhmettima06@gmail.com>
2025-11-20 12:12:27 +0500

    fix firebase api key

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAW7O_...9P58A",
+  apiKey: "AIzaSyAW7O_aEWC32W1k0pVHjA43xcTk6y9P58A",
   authDomain: "skill2skill-702ef.firebaseapp.com",
   projectId: "skill2skill-702ef",
   storageBucket: "skill2skill-702ef.firebasestorage.app",
   messagingSenderId: "401748107712",
   appId: "1:401748107712:web:a45ecf98b182171497a825",
-  measurementId: "G-Y6X27WCM42", // можно оставить, не мешает
+  measurementId: "G-Y6X27WCM42"
 };
 
 const app = initializeApp(firebaseConfig);
